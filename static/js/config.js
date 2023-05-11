22dc378318c42e6d57d20894676647880cac37a7
KevinLin1112 <miragex1112@gmail.com>
2023-05-11 11:43:24 +0800

    設定 config 的參數，提供快速切換功能

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,9 @@
 // 基本 API Host Path, 直接替換 apiHostPath 位置，切換 開發環境 or 正式環境 
-const apiHostPath = 'http://127.0.0.1:5000/api/v1';
+const isProduction = true;
+const localHostPath = 'http://127.0.0.1:5000';
+const productionHostPath = 'https://ticktock.onrender.com';
+const apiVersion = '/api/v1';
+const apiHostPath = (isProduction) ? `${productionHostPath}${apiVersion}` : `${localHostPath}${apiVersion}`;
 
 // 功能 API 完整路徑
 const loginAPI = `${apiHostPath}/auths/login`;
