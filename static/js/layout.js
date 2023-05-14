8fc854664f95b0ade6deb2105c8c335128c60a01
Zoey <pain540145@gmail.com>
2023-05-14 16:48:51 +0800

    共同部分提出layout

---
+++ static/js/layout.js
@@ -0,0 +1,11 @@
+
+$(".header_nav").load("../layout/header_nav.html")
+$(".header_nav_no_logging").load("../layout/header_nav_no_logging.html")
+$(".footer").load("../layout/footer.html")
+$(".user_side").load("../layout/user_side.html")
+$(".personal_center_side").load("../layout/personal_center.html")
+
+
+
+
+
