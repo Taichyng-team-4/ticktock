7f7a209b14f2ab894d61cfb727d8bbd6f09f8fcc
Zoey <pain540145@gmail.com>
2023-06-18 16:42:16 +0800

    活動列表頁、訂單資訊頁串接API

--- docs/js/component_nav.js
+++ docs/js/component_nav.js
@@ -1,58 +1,222 @@
 /** Nav Component **/
 export default {
-  template: `
-  <nav class="bg-navbar ">
-    <div class="container mx-auto px-6 py-3">
-        <div class="mx-auto px-4 py-3 max-w-7xl flex justify-between items-center">
-            <div class="flex items-center">
-                <div>
-                  <a class="text-gray-800 text-xl font-bold hover:text-gray-700"
-                  href="#" @click.prevent="homeClick">Ticktock</a>                        
+  // template: `
+  // <nav class="bg-navbar ">
+  //   <div class="container mx-auto px-6 py-3">
+  //       <div class="mx-auto px-4 py-3 max-w-7xl flex justify-between items-center">
+  //           <div class="flex items-center">
+  //               <div>
+  //                 <a class="text-gray-800 text-xl font-bold hover:text-gray-700"
+  //                 href="#" @click.prevent="homeClick">Ticktock</a>                        
+  //               </div>
+  //               <div class="relative ml-3 flex items-center bg-white rounded-lg">
+  //                   <div class=" left-0">
+  //                       <button type="submit" class="text-black py-1 px-2 rounded-lg ">
+  //                           <span class="material-icons">
+  //                               search
+  //                           </span>
+  //                       </button>
+  //                   </div>
+  //                   <input type="text" class=" py-1 px-2 pl-3 rounded-lg  focus:outline-none"
+  //                       placeholder="  Search"
+  //                       :value="searchData"
+  //                       @click.prevent="searchClick"
+  //                       >
+  //               </div>
+  //           </div>
+  //           <div>
+  //           </div>
+  //           <div>
+  //               <!-- 未登入 -->
+  //               <div class="hidden md:block" v-if="!isLogin">
+  //                   <ul class="flex space-x-4 text-nav  items-center">
+  //                       <li><a href="#" class=" hover:text-gray-200" @click.prevent="switchLoginClick" >登入</a>
+  //                       </li>
+  //                       <li><a href="#" class=" hover:text-gray-200" @click.prevent="switchRegisterClick" >註冊</a>
+  //                       </li>
+  //                   </ul>
+  //               </div>
+
+  //               <!-- 已登入 -->
+  //               <div class="hidden md:block" v-if="isLogin">                
+  //               <ul class="flex space-x-4 text-nav  items-center">
+  //                   <li><a href="#" class=" hover:text-gray-200" @click.prevent="orderQueryClick" >訂單查詢</a></li>
+  //                   <li><a href="#" class=" hover:text-gray-200" @click.prevent="createActivityClick" >建立活動</a></li>
+  //                   <div class="dropdown">
+  //                       <button class="dropbtn flex items-center justify-between">選擇組織
+  //                           <span class="material-icons">
+  //                               arrow_drop_down
+  //                           </span> 
+  //                       </button>
+  //                       <div class="dropdown-content">
+  //                           <p class="ml-4">組織列表</p>
+  //                           <ul class="ml-3">
+  //                               <li v-for="item in orgs"><a href="#">{{ item.name }}</a></li>
+  //                           </ul>
+  //                           <a href="#" @click.prevent="addOrgClick">新增組織</a>
+  //                       </div>
+  //                   </div>
+  //                   <div class="dropdown">
+  //                       <button class="dropbtn">
+  //                           <span class="material-icons">
+  //                               account_circle
+  //                           </span> </button>
+  //                       <div class="dropdown-content">
+  //                           <a href="#" @click.prevent="switchSettingClick">設定</a>
+  //                           <a href="#" @click.prevent="logoutClick">登出</a>
+  //                       </div>
+  //                   </div>
+  //               </ul>
+  //               </div>
+
+  //               <div class="md:hidden flex items-center">
+  //                   <button class="outline-none mobile-menu-button">
+  //                       <span class="material-icons pr-1 text-gray40 cursor-pointer">
+  //                           menu
+  //                       </span>
+  //                   </button>
+  //               </div>
+  //           </div>
+  //       </div>
+  //       <div class="hidden mobile-menu">
+  //           <ul class=" text-nav mx-auto px-4 py-3 max-w-7xl  justify-between items-center">
+  //               <li><a href="#" class=" hover:text-gray-200" @click.prevent="switchLoginClick" >登入</a></li>
+  //               <li><a href="#" class=" hover:text-gray-200" @click.prevent="switchRegisterClick" >註冊</a></li>
+  //           </ul>
+  //       </div>
+  //   </div>
+  // </nav>    
+  // `,
+  template:`
+<nav class="bg-navbar ">
+<div class="container mx-auto px-6 py-2" v-if="!isLogin">
+    <div class="mx-auto px-4 max-w-7xl flex justify-between items-center">
+        <div class="flex items-center">
+            <div>
+                <a class="text-gray-800 text-xl font-bold hover:text-gray-700 cursor-pointer"  @click.prevent="homeClick">Ticktock</a>
+            </div>
+        </div>
+        <div class="w-1/2">
+            <div class="w-full relative ml-3 flex items-center bg-white/[50%] rounded-full">
+                <div class=" left-0">
+                    <button type="submit" class="flex items-center text-black px-2 rounded-full ">
+                        <span class="material-icons">
+                            search
+                        </span>
+                    </button>
                 </div>
-                <div class="relative ml-3 flex items-center bg-white rounded-lg">
-                    <div class=" left-0">
-                        <button type="submit" class="text-black py-1 px-2 rounded-lg ">
-                            <span class="material-icons">
-                                search
+                <input type="text" class=" py-1 px-2 pl-3 rounded-full bg-white/[0] focus:outline-none"
+                    placeholder="Search"
+                    :value="searchData"
+                    @click.prevent="searchClick">
+            </div>
+        </div>
+        <div>
+            <div class="hidden md:block">
+                <ul class="flex space-x-4 text-nav  items-center">
+                    <li><a href="#" class=" hover:text-gray-200" @click.prevent="switchLoginClick" >登入</a>
+                    </li>
+                    <li><a href="#" class=" hover:text-gray-200" @click.prevent="switchRegisterClick" >註冊</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="md:hidden flex items-center">
+                <button class="outline-none mobile-menu-button">
+                    <span class="material-icons pr-1 text-gray40 cursor-pointer">
+                        menu
+                    </span>
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="hidden mobile-menu">
+        <ul class=" text-nav mx-auto px-4 py-3 max-w-7xl  justify-between items-center">
+            <li><a href="#" class=" hover:text-gray-200" @click.prevent="switchLoginClick" >登入</a></li>
+            <li><a href="#" class=" hover:text-gray-200" @click.prevent="switchRegisterClick" >註冊</a></li>
+        </ul>
+    </div>
+</div>
+
+
+<div class="container mx-auto px-6 py-2" v-if="isLogin">
+            <div class="mx-auto px-4 max-w-7xl flex justify-between items-center">
+                <div class="flex items-center">
+                    <div>
+                        <a class="text-gray-800 text-xl font-bold hover:text-gray-700 cursor-pointer"  @click.prevent="homeClick">Ticktock</a>
+                    </div>
+                </div>
+                <div class="w-1/2">
+                    <div class="w-full relative ml-3 flex items-center bg-white/[50%] rounded-full">
+                        <div class=" left-0">
+                            <button type="submit" class="flex items-center text-black px-2 rounded-full ">
+                                <span class="material-icons">
+                                    search
+                                </span>
+                            </button>
+                        </div>
+                        <input type="text" class=" py-1 px-2 pl-3 rounded-full bg-white/[0] focus:outline-none"
+                            placeholder="Search"
+                            :value="searchData"
+                            @click.prevent="searchClick">
+                    </div>
+                </div>
+                <div>
+                    <div class="hidden md:block">
+                        <ul class="flex space-x-4 text-nav  items-center">
+                            <li><a href="#" class=" hover:text-gray-200" @click.prevent="orderQueryClick">訂單查詢</a></li>
+                            <li><a href="#" class=" hover:text-gray-200" @click.prevent="createActivityClick">建立活動</a></li>
+                            <div class="dropdown">
+                                <button class="dropbtn flex items-center justify-between">選擇組織
+                                    <span class="material-icons">
+                                        arrow_drop_down
+                                    </span> 
+                                </button>
+                                <div class="dropdown-content">
+                                    <p class="ml-4">組織列表</p>
+                                    <ul class="ml-3">
+                                        <li v-for="item in orgs"><a href="#">{{ item.name }}</a></li>
+                                    </ul>
+                                    <a href="#" @click.prevent="addOrgClick">新增組織</a>
+                                </div>
+                            </div>
+                            <div class="dropdown">
+                                <button class="dropbtn">
+                                    <span class="material-icons">
+                                        account_circle
+                                    </span> </button>
+                                <div class="dropdown-content">
+                                    <a href="#" @click.prevent="switchSettingClick">設定</a>
+                                    <a href="#" @click.prevent="logoutClick">登出</a>
+                                </div>
+                            </div>
+                        </ul>
+                    </div>
+
+                    <div class="md:hidden flex items-center">
+                        <button class="outline-none mobile-menu-button">
+                            <span class="material-icons pr-1 text-gray40 cursor-pointer">
+                                menu
                             </span>
                         </button>
                     </div>
-                    <input type="text" class=" py-1 px-2 pl-3 rounded-lg  focus:outline-none"
-                        placeholder="  Search"
-                        :value="searchData"
-                        @click.prevent="searchClick"
-                        >
                 </div>
             </div>
-            <div>
-            </div>
-            <div>
-                <!-- 未登入 -->
-                <div class="hidden md:block" v-if="!isLogin">
-                    <ul class="flex space-x-4 text-nav  items-center">
-                        <li><a href="#" class=" hover:text-gray-200" @click.prevent="switchLoginClick" >登入</a>
-                        </li>
-                        <li><a href="#" class=" hover:text-gray-200" @click.prevent="switchRegisterClick" >註冊</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <!-- 已登入 -->
-                <div class="hidden md:block" v-if="isLogin">                
-                <ul class="flex space-x-4 text-nav  items-center">
-                    <li><a href="#" class=" hover:text-gray-200" @click.prevent="orderQueryClick" >訂單查詢</a></li>
-                    <li><a href="#" class=" hover:text-gray-200" @click.prevent="createActivityClick" >建立活動</a></li>
+            <div class="hidden mobile-menu">
+                <ul class=" text-nav mx-auto px-4 py-3 max-w-7xl  justify-between items-center">
+                    <li><a href="#" class=" hover:text-gray-200" @click.prevent="orderQueryClick">訂單查詢</a></li>
+                    <li><a href="#" class=" hover:text-gray-200" @click.prevent="createActivityClick">建立活動</a></li>
                     <div class="dropdown">
                         <button class="dropbtn flex items-center justify-between">選擇組織
                             <span class="material-icons">
                                 arrow_drop_down
-                            </span> 
-                        </button>
-                        <div class="dropdown-content">
+                            </span> </button>
+                        <div class="subdropdown-content">
                             <p class="ml-4">組織列表</p>
                             <ul class="ml-3">
                                 <li v-for="item in orgs"><a href="#">{{ item.name }}</a></li>
                             </ul>
+                            <hr>
                             <a href="#" @click.prevent="addOrgClick">新增組織</a>
                         </div>
                     </div>
@@ -60,32 +224,20 @@ export default {
                         <button class="dropbtn">
                             <span class="material-icons">
                                 account_circle
+                            </span>
+                            <span class="material-icons">
+                                arrow_drop_down
                             </span> </button>
-                        <div class="dropdown-content">
+                        <div class="subdropdown-content">
                             <a href="#" @click.prevent="switchSettingClick">設定</a>
                             <a href="#" @click.prevent="logoutClick">登出</a>
                         </div>
                     </div>
                 </ul>
-                </div>
-
-                <div class="md:hidden flex items-center">
-                    <button class="outline-none mobile-menu-button">
-                        <span class="material-icons pr-1 text-gray40 cursor-pointer">
-                            menu
-                        </span>
-                    </button>
-                </div>
             </div>
         </div>
-        <div class="hidden mobile-menu">
-            <ul class=" text-nav mx-auto px-4 py-3 max-w-7xl  justify-between items-center">
-                <li><a href="#" class=" hover:text-gray-200" @click.prevent="switchLoginClick" >登入</a></li>
-                <li><a href="#" class=" hover:text-gray-200" @click.prevent="switchRegisterClick" >註冊</a></li>
-            </ul>
-        </div>
-    </div>
-  </nav>    
+</nav>
+
   `,
   data() {
     return {
@@ -143,103 +295,3 @@ export default {
   },
 
 }
-
-{/* <nav class="bg-navbar">
-<div class="container mx-auto px-6 py-3">
-  <div
-    class="mx-auto px-4 py-3 max-w-7xl flex justify-between items-center"
-  >
-    <div class="flex items-center">
-      <div>
-        <a
-          class="text-gray-800 text-xl font-bold hover:text-gray-700"
-          href="../index.html"
-          >Ticktock</a
-        >
-      </div>
-      <div class="relative ml-3 flex items-center bg-white rounded-lg">
-        <div class="left-0">
-          <button type="submit" class="text-black py-1 px-2 rounded-lg">
-            <span class="material-icons"> search </span>
-          </button>
-        </div>
-        <input
-          type="text"
-          class="py-1 px-2 pl-3 rounded-lg focus:outline-none"
-          placeholder="  Search"
-        />
-      </div>
-    </div>
-    <div></div>
-    <div>
-      <div class="hidden md:block">
-        <ul class="flex space-x-4 text-nav items-center">
-          <li><a href="#" class="hover:text-gray-200">訂單查詢</a></li>
-          <li><a href="#" class="hover:text-gray-200">建立活動</a></li>
-          <div class="dropdown">
-            <button class="dropbtn flex items-center justify-between">
-              選擇組織
-              <span class="material-icons"> arrow_drop_down </span>
-            </button>
-            <div class="dropdown-content">
-              <p class="ml-4">組織列表</p>
-              <ul class="ml-3">
-                <li><a href="#">Link 1</a></li>
-              </ul>
-              <a href="#">新增組織</a>
-            </div>
-          </div>
-          <div class="dropdown">
-            <button class="dropbtn">
-              <span class="material-icons"> account_circle </span>
-            </button>
-            <div class="dropdown-content">
-              <a href="#">設定</a>
-              <a href="#">登出</a>
-            </div>
-          </div>
-        </ul>
-      </div>
-
-      <div class="md:hidden flex items-center">
-        <button class="outline-none mobile-menu-button">
-          <span class="material-icons pr-1 text-gray40 cursor-pointer">
-            menu
-          </span>
-        </button>
-      </div>
-    </div>
-  </div>
-  <div class="hidden mobile-menu">
-    <ul
-      class="text-nav mx-auto px-4 py-3 max-w-7xl justify-between items-center"
-    >
-      <li><a href="#" class="hover:text-gray-200">訂單查詢</a></li>
-      <li><a href="#" class="hover:text-gray-200">建立活動</a></li>
-      <div class="dropdown">
-        <button class="dropbtn flex items-center justify-between">
-          選擇組織 <span class="material-icons"> arrow_drop_down </span>
-        </button>
-        <div class="subdropdown-content">
-          <p class="ml-4">組織列表</p>
-          <ul class="ml-3">
-            <li><a href="#">Link 1</a></li>
-          </ul>
-          <hr />
-          <a href="#">新增組織</a>
-        </div>
-      </div>
-      <div class="dropdown">
-        <button class="dropbtn">
-          <span class="material-icons"> account_circle </span>
-          <span class="material-icons"> arrow_drop_down </span>
-        </button>
-        <div class="subdropdown-content">
-          <a href="#">設定</a>
-          <a href="#">登出</a>
-        </div>
-      </div>
-    </ul>
-  </div>
-</div>
-</nav> */}
--- docs/js/config.js
+++ docs/js/config.js
@@ -1,32 +1,37 @@
-// 基本 API Host Path, 直接替換 apiHostPath 位置，切換 開發環境 or 正式環境 
+// 基本 API Host Path, 直接替換 apiHostPath 位置，切換 開發環境 or 正式環境
 const isProduction = true;
-const localHostPath = 'http://127.0.0.1:5000';
-const productionHostPath = 'https://ticktock.onrender.com';
-const apiVersion = '/api/v1';
-const apiHostPath = (isProduction) ? `${productionHostPath}${apiVersion}` : `${localHostPath}${apiVersion}`;
+const localHostPath = "http://127.0.0.1:5000";
+const productionHostPath = "https://ticktock.onrender.com";
+const apiVersion = "/api/v1";
+const apiHostPath = isProduction ? `${productionHostPath}${apiVersion}` : `${localHostPath}${apiVersion}`;
 
 // 功能 API 完整路徑
 const loginAPI = `${apiHostPath}/auths/login`;
 const registerAPI = `${apiHostPath}/auths/signup`;
 const forgetPasswordAPI = `${apiHostPath}/auths/password/forgot`;
 const activityAPI = `${apiHostPath}/activities/`;
-// 取得 以及 更新 User Profile 
+const venuesAPI = `${apiHostPath}/venues/`;
+const ordersMeAPI = `${apiHostPath}/orders/me`;
+const usersAPI = `${apiHostPath}/users/`;
+// 取得 以及 更新 User Profile
 const userProfileAPI = `${apiHostPath}/profile`;
 // 新增組織
 const orgsAPI = `${apiHostPath}/orgs`;
 
 // 預設 Avatar
-const defaultAvatar = 'https://picsum.photos/100/100';
+const defaultAvatar = "https://picsum.photos/100/100";
 
 const config = {
-  apiHostPath,
-  loginAPI,
-  registerAPI,
-  forgetPasswordAPI,
-  activityAPI,
-  userProfileAPI,
-  orgsAPI,
-  defaultAvatar
+    apiHostPath,
+    loginAPI,
+    registerAPI,
+    forgetPasswordAPI,
+    activityAPI,
+    userProfileAPI,
+    orgsAPI,
+    defaultAvatar,
+    ordersMeAPI,
+    usersAPI,
 };
 
 export default config;
